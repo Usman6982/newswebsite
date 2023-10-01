725c4b4eeeb808294f83d953ec802b7e1a949508
Usman6982 <falakudaas4051@gmail.com>
2023-10-01 07:10:24 +0500

    Component structure and navBar

---
+++ src/components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        This is newsItem2
+      </div>
+    )
+  }
+}
+
+export default NewsItem
