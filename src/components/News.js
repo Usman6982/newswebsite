c3fbd8f012eff2571b4f1c425753a002b6ff3461
Usman6982 <falakudaas4051@gmail.com>
2023-10-07 12:03:17 +0500

    Refactoring News Component

--- src/components/News.js
+++ src/components/News.js
@@ -26,45 +26,27 @@ export class News extends Component {
             page: 1,
         };
     }
-    async componentDidMount() {
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a38c7916cd96416f88991edd14b4f661&page=1&pageSize=${this.props.pageSize}`;
+    async updateNews(){
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a38c7916cd96416f88991edd14b4f661&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
         let data = await fetch(url);//promise//
         let parsedData = await data.json()
-        console.log(parsedData);
         this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults, loading: false })
 
     }
+    async componentDidMount() {
+      this.updateNews();
+    }
 
     handlePrevClick = async () => {
-        console.log("Previous");
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a38c7916cd96416f88991edd14b4f661&page=${this.state.page + 1}pageSize=${this.props.pageSize}`;
-        this.setState({ loading: true })
-        let data = await fetch(url);//promise//
-        let parsedData = await data.json()
-        console.log(parsedData);
-        this.setState({
-            page: this.state.page - 1,
-            articles: parsedData.articles,
-            loading: false
-        })
+         this.setState({page:this.state.page - 1})
+        this.updateNews();
     }
 
     handleNextClick = async () => {
-        console.log("Next");
-        if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) //it will return the next  big integer//
-        {
-            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a38c7916cd96416f88991edd14b4f661&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-            this.setState({ loading: true })
-            let data = await fetch(url);//promise//
-            let parsedData = await data.json()
-            this.setState({ loading: false })
-            this.setState({
-                page: this.state.page + 1,
-                articles: parsedData.articles,
-                loading: false
-            })
-        }
+        this.setState({page:this.state.page + 1});
+        this.updateNews();
+        // }
     }
 
     render() {
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -9,7 +9,7 @@ export class NewsItem extends Component {
         return (
             <div className="my-3">
                 <div className="card" style={{ width: '18rem' }}>
-                <span class="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex:'1'}}>
+                <span className="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex:'1'}}>
                 {source}</span>
                     <img src={!imageUrl ? "https://img.onmanorama.com/content/dam/mm/en/news/india/images/2023/10/1/nps-protest-ramlila-maidan.jpg" : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
