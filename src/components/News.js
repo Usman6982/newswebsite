15359bf0a5a17dbcafc26df6ffd1ac60c5e8be31
Usman6982 <falakudaas4051@gmail.com>
2023-10-08 02:55:41 +0500

    Adding a top loading bar

--- src/components/News.js
+++ src/components/News.js
@@ -34,12 +34,16 @@ export class News extends Component {
         document.title = `${this.capitalizeFirstLetter(this.props.category)}-NewsMonkey`;
     }
     async updateNews() {
+        this.props.setProgress(10);
         const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a38c7916cd96416f88991edd14b4f661&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
         let data = await fetch(url);//promise//
+        this.props.setProgress(30);
         let parsedData = await data.json()
-        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults, loading: false
+        this.props.setProgress(70);
+        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults, loading: false,
         })
+        this.props.setProgress(100);
 
     }
     async componentDidMount() {
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -9,8 +9,16 @@ export class NewsItem extends Component {
         return (
             <div className="my-3">
                 <div className="card" style={{ width: '18rem' }}>
-                <span className="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex:'1'}}>
-                {source}</span>
+                <div style={
+                    {display:'flex',
+                    justifyContent:'flex-end',
+                    position:'absolute',
+                    right:'0'
+                    }
+                }>
+                <span className="badge rounded-pill bg-danger">
+                {source}</span>  
+                </div>
                     <img src={!imageUrl ? "https://img.onmanorama.com/content/dam/mm/en/news/india/images/2023/10/1/nps-protest-ramlila-maidan.jpg" : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title} 
